120904b7d10a0a8836ffaa63808937493bbb93b5
Fabio Mereu <fabio@mereu.info>
2019-01-14 11:05:38 +0100

    fix NaN test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,30 @@
 var assert = require("assert");
 var C = require("../lib/conject");
 
-describe("C {} should be loaded", function() {
+function getString(variable) {
 
+  if (variable === null)
+    return "[null]";
+
+  if (typeof variable === "undefined")
+    return "[undefined]";
+
+  if (typeof value === "number" && isNaN(value))
+    return "[NaN]";
+
+  if (typeof variable === "string")
+        return "[string] '" + variable + "'";
+
+  if (typeof variable === "object" && variable.constructor.name === "Object")
+    return "[object] " + JSON.stringify(variable);
+
+  if (typeof variable === "object")
+    return "[" + variable.constructor.name + "] " + String(variable);
+
+  return  "[" + typeof variable + "] " + String(variable);
+}
+
+describe("C {} should be loaded", function() {
   it("C should be an object", function() {
     assert.deepStrictEqual(typeof C, "object");
   });
@@ -10,87 +32,145 @@ describe("C {} should be loaded", function() {
   it("C.Chainable should be a constructor", function() {
     assert.deepStrictEqual(typeof C.Chainable, "function");
   });
+});
 
-  var starters = ["if", "and", "or", "xor", "nand", "nor", "xnor",
-    "anda", "ora", "xora", "nanda", "nora", "xnora"];
+var starters = ["if", "and", "or", "xor", "nand", "nor", "xnor",
+  "anda", "ora", "xora", "nanda", "nora", "xnora",
+  "throw"];
 
+describe("C should include starters", function() {
   starters.forEach(function (operator) {
     it("C." + operator + " is a Function", function() {
       assert.deepStrictEqual(typeof C[operator], "function");
     });
   });
+});
 
-  var operators = starters.concat(["in", "out", "atmost", "during"]);
+var operators = starters.concat(["in", "out", "atmost", "during"]);
 
+describe("new Chainable should have all methods", function() {
   operators.forEach(function (operator) {
     it("(new Chainable())." + operator + " is a Function", function() {
       assert.deepStrictEqual(typeof (new C.Chainable ())[operator], "function");
     });
   });
-
 });
+
+
+function testValue(description, getterC, value) {
+  describe(description, function() {
+
+    it(".value === " + getString(value), function() {
+
+      var evaluation = getterC().run();
+
+      if (typeof value === "number" && isNaN(value)) {
+        assert.ok(isNaN(evaluation.value));
+      }
+      else {
+        assert.deepStrictEqual(evaluation.value, value);
+      }
+    });
+
+    testOnTrue(getterC, !!value);
+    testOnFalse(getterC, !value);
+    testOnError(getterC, false);
+  });
+}
+
+function testReason(description, getterC, reason) {
+  describe(description, function() {
+    it(".run()" , function() {
+      assert.throws(
+        function () {
+          getterC().run();
+        },
+
+        function (error) {
+          return error === reason;
+        }
+      );
+    });
+
+  //  testOnTrue(getterC, !!value);
+  //  testOnFalse(getterC, !value);
+  //testOnError(getterC, false);
+  });
+}
+
+function testOnTrue(getterC, shouldOnTrue) {
+  it(".onTrue() was" + (shouldOnTrue ? " " : " NOT ") + "called", function() {
+    var wasCalled = false;
+    getterC().onTrue(function () {wasCalled = true}).run();
+    assert.deepStrictEqual(wasCalled, !!shouldOnTrue);
+  });
+}
+
+function testOnFalse(getterC, shouldOnFalse) {
+  it(".onFalse() was" + (shouldOnFalse ? " " : " NOT ") + "called", function() {
+    var wasCalled = false;
+    getterC().onFalse(function () {wasCalled = true}).run();
+    assert.deepStrictEqual(wasCalled, !!shouldOnFalse);
+  });
+}
+
+function testOnError(getterC, shouldOnError) {
+  it(".onError() was" + (shouldOnError ? " " : " NOT ") + "called", function() {
+    var wasCalled = false;
+    getterC().onError(function () {wasCalled = true}).run();
+    assert.deepStrictEqual(wasCalled, !!shouldOnError);
+  });
+}
 
 
 describe("Basic syncrhonous evaluation", function() {
 
-  function testSingle (value) {
-
-    var strValue = typeof value === "object" ? JSON.stringify(value) : String(value);
-
-    describe("C.if(" + strValue + ")", function() {
-
-      var bool = !!value;
-      var onTrue = false, onFalse = false, onError = false;
-
-      var evaluation = C
-        .if(value)
-        .onTrue(function () {onTrue = true;})
-        .onFalse(function () {onFalse = true;})
-        .onError(function () {onError = true;})
-        .run();
-
-      it(".value === " + strValue, function() {
-        if (isNaN(value)) {
-          assert.ok(isNaN(evaluation.value));
-        }
-
-        else {
-          assert.deepStrictEqual(evaluation.value, value);
-        }
-      });
-
-      it(".onTrue() was" + (bool ? " " : " not ") + "called", function() {
-        assert.deepStrictEqual(onTrue, bool);
-      });
-
-      it(".onFalse() was" + (bool ?  " not " : " ") + "called", function() {
-        assert.deepStrictEqual(onFalse, !bool);
-      });
-
-      it(".onError() was never called", function() {
-        assert.deepStrictEqual(onError, false);
-      });
-
-    });
-  }
-
   var truthy = ["Hello", 123.45, true, {yes: 1}, new Date()];
-
   var falsy = [false, null, 0, "", undefined, NaN];
 
-  truthy.forEach(testSingle);
+  truthy.concat(falsy).forEach(function (value) {
+    var description = "C.if(" + getString(value) + ")";
 
-  falsy.forEach(testSingle);
+    testValue(
+      description,
 
-});
+      function () {
+        return C.if(value);
+      },
 
+      value
+    );
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    // pending test below
-    it('should return -1 when the value is not present');
   });
+
+  describe("rejecting the evaluation - expects an error", function () {
+
+    var throwns = ["Failure", new Error("What's happened"), .0001];
+
+    throwns.forEach(function (reason) {
+      var description = "C.throw(" + getString(reason) + ")";
+
+      testReason(
+        description,
+
+        function () {
+          return C.throw(reason);
+        },
+        reason
+      );
+    });
+  });
+
+
+
+
+
+//  throwns.forEach(testSingle);
+
 });
+
+
+
 
 
 // https://shields.io/#/examples/platform-support
